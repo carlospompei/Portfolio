a472c5542f3a51826dbfe5a43bf4966159108163
Carlos Pompei <102166252+carlospompei@users.noreply.github.com>
2022-05-21 19:24:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// efeito de escrever na descrição. / Colocar logo do instagram e github. / quando passar o mouse ela aumenta de tamanho
+function mudaTema() {
+  document.body.classList.toggle("dark");
+}
